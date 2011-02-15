0cf4c279b1228d8f0efb50a4cd64a49222976f24
Greg Jordan <gjuggler@gmail.com>
2011-02-15 11:44:43 +0000

    Add round-trip tests for tree IO.

---
+++ test/tree_io.R
@@ -0,0 +1,25 @@
+# tree_io.R - a series of round-trip tests for ape's phylogenetic tree I/O.
+#
+# Author: Gregory Jordan (greg@ebi.ac.uk)
+#
+# Instructions: use the following command from within the ape base directory:
+# > R --vanilla < test/tree_io.R
+
+library(ape)
+
+round.trip <- function(str.in) {
+  phylo <- read.tree(text = str.in)
+  str.out <- write.tree(phylo)
+  if ((!identical(str.in, str.out))) {
+    print(paste('Round trip failed!', "Expected [", str.in, "] Got [", str.out,"]", sep=''))
+  }
+}
+round.trip("(a,(b,c));")
+round.trip("(a,b,c,d,e,f,g);")
+round.trip("(a,(b,c,d));")
+round.trip("(a,(b,(c,d)e)f)g;")
+round.trip("(a:1,(b:1,c:1):1):1;")
+round.trip("(a,b);")
+round.trip("(a,(b,c));")
+round.trip("(a,(b));")
+round.trip("((a,b));")
